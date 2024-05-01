d61d0920845c9f375f90c5f55ca776ef864cd039
thusy <web.dev418@gmail.com>
2024-05-01 19:34:42 +0200

    feat : search recipes with for loop

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -7,3 +7,41 @@ recipes.forEach(recipe => {
     let recipeCard = recipeTemplate(recipe);
     recipeContainer.appendChild(recipeCard);
 });
+
+function searchRecipes(searchTerms) {
+    let results = [];
+
+    for (let i = 0; i < recipes.length; i++) {
+        let recipe = recipes[i];
+
+        if (recipe.name.toLowerCase().includes(searchTerms.toLowerCase())) {
+            results.push(recipe);
+        }
+
+        for (let j = 0; j < recipe.ingredients.length; j++) {
+            if (recipe.ingredients[j].ingredient.toLowerCase().includes(searchTerms.toLowerCase())) {
+                results.push(recipe);
+                break;
+            }
+        }
+    }
+
+    return results;
+}
+
+let searchForm = document.querySelector('.search_form');
+let searchInput = document.querySelector('.search_input');
+
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let searchTerms = searchInput.value;
+    let results = searchRecipes(searchTerms);
+
+    recipeContainer.innerHTML = '';
+
+    results.forEach(recipe => {
+        let recipeCard = recipeTemplate(recipe);
+        recipeContainer.appendChild(recipeCard);
+    });
+});
