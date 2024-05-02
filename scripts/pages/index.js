78f0ffebbeee4911cdf8fe462010703bb1ca7fa4
thusy <web.dev418@gmail.com>
2024-05-02 15:52:35 +0200

    feat : search function

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -7,3 +7,38 @@ recipes.forEach(recipe => {
     let recipeCard = recipeTemplate(recipe);
     recipeContainer.appendChild(recipeCard);
 });
+
+function searchRecipes(searchTerms) {
+    let results = [];
+
+    recipes.forEach(recipe => {
+        if (recipe.name.toLowerCase().includes(searchTerms.toLowerCase())) {
+            results.push(recipe);
+        } else {
+            recipe.ingredients.forEach(ingredient => {
+                if (ingredient.ingredient.toLowerCase().includes(searchTerms.toLowerCase())) {
+                    results.push(recipe);
+                }
+            });
+        }
+    });
+
+    return results;
+}
+
+let searchForm = document.querySelector('.search_form');
+let searchInput = document.querySelector('.search_input');
+
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    let searchTerms = searchInput.value;
+    let results = searchRecipes(searchTerms);
+
+    recipeContainer.innerHTML = '';
+
+    results.forEach(recipe => {
+        let recipeCard = recipeTemplate(recipe);
+        recipeContainer.appendChild(recipeCard);
+    });
+});
